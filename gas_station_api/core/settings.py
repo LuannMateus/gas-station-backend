c531707c3828942293596249899530e23846c90a
LuannMateus <luanmateuslima34@gmail.com>
2022-04-24 09:22:17 -0300

    refactor: change project name to core

--- gas_station_api/core/settings.py
+++ gas_station_api/core/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'gas_station_api.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gas_station_api.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
